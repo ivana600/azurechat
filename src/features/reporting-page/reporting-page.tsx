6c07496916a2ae9128ac323f8292ce11f7d1802d
ivana600 <121664767+ivana600@users.noreply.github.com>
2025-04-11 18:58:26 -0500

    Update reporting-page.tsx
    reporting removal1

--- src/features/reporting-page/reporting-page.tsx
+++ src/features/reporting-page/reporting-page.tsx
@@ -1,89 +1,15 @@
-import { ChevronLeft, ChevronRight } from "lucide-react";
-import Link from "next/link";
-import { FC, Suspense } from "react";
-import { Button } from "../ui/button";
-import { DisplayError } from "../ui/error/display-error";
-import { PageLoader } from "../ui/page-loader";
-import { ScrollArea } from "../ui/scroll-area";
-import {
-  Table,
-  TableBody,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "../ui/table";
-import { ReportingHero } from "./reporting-hero";
-import { FindAllChatThreadsForAdmin } from "./reporting-services/reporting-service";
-import ChatThreadRow from "./table-row";
+"use client";
+import { Hero } from "@/features/ui/hero";
+import { Sheet } from "lucide-react";
 
-const SEARCH_PAGE_SIZE = 100;
-
-interface ChatReportingProps {
-  page: number;
-}
-
-export const ChatReportingPage: FC<ChatReportingProps> = async (props) => {
+export const ReportingHero = () => {
   return (
-    <ScrollArea className="flex-1">
-      <main className="flex flex-1 flex-col">
-        <ReportingHero />
-        <Suspense fallback={<PageLoader />} key={props.page}>
-          <ReportingContent {...props} />
-        </Suspense>
-      </main>
-    </ScrollArea>
+    <Hero
+      title={
+        <>
+          <Sheet size={36} strokeWidth={1.5} />
+          Chat reports for FlayerGPT has been disabled for admins to keep Slex's thoughts and communications private. Oh, and user inquires private too, I guess, the LnL lawyers tell me...
+        </>
+    ></Hero>
   );
 };
-
-async function ReportingContent(props: ChatReportingProps) {
-  let pageNumber = props.page < 0 ? 0 : props.page;
-  let nextPage = pageNumber + 1;
-  let previousPage = pageNumber - 1;
-
-  const chatHistoryResponse = await FindAllChatThreadsForAdmin(
-    SEARCH_PAGE_SIZE,
-    props.page * SEARCH_PAGE_SIZE
-  );
-
-  if (chatHistoryResponse.status !== "OK") {
-    return <DisplayError errors={chatHistoryResponse.errors} />;
-  }
-
-  const chatThreads = chatHistoryResponse.response;
-  const hasMoreResults = chatThreads.length === SEARCH_PAGE_SIZE;
-  return (
-    <div className="container max-w-4xl py-3">
-      <Table>
-        <TableHeader>
-          <TableRow>
-            <TableHead>Conversation</TableHead>
-            <TableHead className="w-[200px]">User</TableHead>
-            <TableHead className="w-[100px]">Date</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {chatThreads &&
-            chatThreads.map((chatThread) => (
-              <ChatThreadRow key={chatThread.id} {...chatThread} />
-            ))}
-        </TableBody>
-      </Table>
-      <div className="flex gap-2 p-2 justify-end">
-        {previousPage >= 0 && (
-          <Button asChild size={"icon"} variant={"outline"}>
-            <Link href={"/reporting?pageNumber=" + previousPage}>
-              <ChevronLeft />
-            </Link>
-          </Button>
-        )}
-        {hasMoreResults && (
-          <Button asChild size={"icon"} variant={"outline"}>
-            <Link href={"/reporting?pageNumber=" + nextPage}>
-              <ChevronRight />
-            </Link>
-          </Button>
-        )}
-      </div>
-    </div>
-  );
-}
