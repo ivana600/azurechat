e9a83e8f0276ea6a25710a944a2845fb9c00976f
ivana600 <121664767+ivana600@users.noreply.github.com>
2025-04-11 19:14:53 -0500

    Update reporting-page.tsx
    reporting disable 2

--- src/features/reporting-page/reporting-page.tsx
+++ src/features/reporting-page/reporting-page.tsx
@@ -1,15 +1,15 @@
-"use client";
-import { Hero } from "@/features/ui/hero";
-import { Sheet } from "lucide-react";
-
 export const ReportingHero = () => {
   return (
     <Hero
       title={
         <>
           <Sheet size={36} strokeWidth={1.5} />
-          Chat reports for FlayerGPT has been disabled for admins to keep Slex's thoughts and communications private. Oh, and user inquires private too, I guess, the LnL lawyers tell me...
+          Chat Report
         </>
+      }
+      description={
+        "Chat reports for FlayerGPT has been disabled for admins to keep Slex's thoughts and communications private. Oh, and user inquires private too, I guess, the LnL lawyers tell me..."
+      }
     ></Hero>
   );
 };
